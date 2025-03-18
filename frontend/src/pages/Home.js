06c24c23a90accb4ea3c2d8e50dbca7810e17f22
Adrián Pastor Morata <adrian.p.morata@gmail.com>
2025-03-18 22:08:04 +0100

    Ajustes en la plantilla: dependencias y archivos package ordenados en cada carpeta.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,10 +4,7 @@ function Home() {
   const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
-    // Usamos la variable de entorno para la URL del backend
-    const apiUrl = process.env.REACT_APP_API_URL;
-
-    fetch(`${apiUrl}/api/usuarios`) // Llamar a la API del backend con la URL parametrizada
+    fetch("http://localhost:5000/api/usuarios") // Llamar a la API del backend
       .then(response => response.json())
       .then(data => setUsuarios(data))
       .catch(error => console.error("Error al obtener usuarios:", error));
