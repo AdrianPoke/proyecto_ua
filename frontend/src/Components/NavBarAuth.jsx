195ea2e0bd7687715b4b25d10edcc7a9137e6695
Christopher Josué Pérez Castro <cjpc3@mscloud.ua.es>
2025-05-01 19:27:56 +0200

    Barra de navegación responsive

--- frontend/src/Components/NavBarAuth.jsx
+++ frontend/src/Components/NavBarAuth.jsx
@@ -1,37 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch, FaPlusCircle } from "react-icons/fa";
-import "../styles/NavBarAuth.css"; // Importa los estilos
+import "../styles/NavBarAuth.css";
 import logo from "../logo.png";
 import pr from "../icons/profile.png";
 
-
 function NavBarAuth() {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  useEffect(() => {
+    const cerrar = () => setMenuAbierto(false);
+    window.addEventListener("scroll", cerrar);
+    return () => window.removeEventListener("scroll", cerrar);
+  }, []);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/" className="logo">
-        <img src={logo} alt="Logo" />
-        </Link>
-        <div className="search-bar">
-          <input type="text" placeholder="Inserte palabras clave ..." />
-          <button className="search-button">
-            <FaSearch />
-          </button>
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <Link to="/" className="logo">
+            <img src={logo} alt="Logo" />
+          </Link>
+          <div className="search-bar">
+            <input type="text" placeholder="Inserte palabras clave ..." />
+            <button className="search-button">
+              <FaSearch />
+            </button>
+          </div>
         </div>
+
+        {/* Menú horizontal (escritorio) */}
+        <div className="navbar-right">
+          <Link to="/" className="nav-link">Inicio</Link>
+          <Link to="/subir-assets" className="nav-link">
+            <FaPlusCircle className="icon" /> Subir Assets
+          </Link>
+          <Link to="/categorias" className="nav-link">Categorías</Link>
+          <Link to="/busqueda-avanzada" className="nav-link">Búsqueda Avanzada</Link>
+          <Link to="/perfil" className="profile-icon">
+            <img src={pr} alt="Perfil" />
+          </Link>
+        </div>
+
+        {/* Botón hamburguesa (solo móvil) */}
+        <button className="hamburguesa" onClick={() => setMenuAbierto(!menuAbierto)}>
+          ☰
+        </button>
+      </nav>
+
+      {/* Sidebar lateral (móvil) */}
+      <div className={`sidebar ${menuAbierto ? "activo" : ""}`}>
+        <Link to="/" onClick={() => setMenuAbierto(false)}>Inicio</Link>
+        <Link to="/subir-assets" onClick={() => setMenuAbierto(false)}>Subir Assets</Link>
+        <Link to="/categorias" onClick={() => setMenuAbierto(false)}>Categorías</Link>
+        <Link to="/busqueda-avanzada" onClick={() => setMenuAbierto(false)}>Búsqueda Avanzada</Link>
+        <Link to="/perfil" onClick={() => setMenuAbierto(false)}>Perfil</Link>
       </div>
-  
-      <div className="navbar-right">
-        <Link to="/subir-assets" className="nav-link">
-          <FaPlusCircle className="icon" /> Subir Assets
-        </Link>
-        <Link to="/categorias" className="nav-link">Categorías</Link>
-        <Link to="/busqueda-avanzada" className="nav-link">Búsqueda Avanzada</Link>
-        <Link to="/perfil" className="profile-icon">
-        <img src={pr} alt="Profile" />
-        </Link>
-      </div>
-    </nav>
+
+      {/* Fondo que cierra menú al hacer clic */}
+      {menuAbierto && <div className="sidebar-overlay" onClick={() => setMenuAbierto(false)} />}
+    </>
   );
 }
 
